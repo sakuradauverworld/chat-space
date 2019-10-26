1fdf433a100c796dda55fc925b3b0ca8850e859a
sakuradauverworld <sakuradaasaku@gmail.com>
2019-10-26 21:40:07 +0900

    iiiii

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,56 @@
+$(document).on('turbolinks:load', function(){
+
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="main3">
+                  <div class="main2"></div>
+                  <div class="main3__user">
+                    ${message.user_name}
+                  </div>
+                  <div class="main3__date">
+                    ${message.date}
+                  </div>
+                  <div class="main3__chat">
+                    <p class="lower-message__content">
+                    ${content}
+                    </p>
+                    ${img}
+                  </div>
+                </div>`
+  return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var message = new FormData(this);
+    var url = (window.location.href);
+    $.ajax({  
+      url: url,
+      type: 'POST',
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      console.log(data)
+      var html = buildHTML(data);
+      $('.main4').append(html);
+      $('#message_content').val('');
+      $('#message_content').val('');
+      var target = $('.main3').last();
+      var position = target.offset().top + $('.main5').scrollTop();
+
+      $('.main5').animate({
+        scrollTop: position
+      }, 300, 'swing');
+    })
+    .fail(function(data){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
+    })
+    .always(function(data){
+      $('.form__submit').prop('disabled', false);
+    })
+  })
+})
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,15 +8,26 @@ class MessagesController < ApplicationController
   end
 
   def create
-  @message = @group.messages.new(message_params)
+    @message = @group.messages.new(message_params)
+    
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-    else
-      @messages = @group.messages.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください。'
-      render :index
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(params[:group_id]) }
+        format.json
+      end
     end
   end
+
+  # def create
+  #   @message = @group.messages.new(message_params)
+  #   if @message.save
+  #     redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+  #   else
+  #     @messages = @group.messages.includes(:user)
+  #     flash.now[:alert] = 'メッセージを入力してください。'
+  #     render :index
+  #   end
+  # end
 
   private
 
