a120b595fb586c24f8d11c7ef5a7021586425fc0
sakuradauverworld <sakuradaasaku@gmail.com>
2019-11-01 11:09:14 +0900

    q

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,19 +9,16 @@ set :repo_url,  'git@github.com:sakuradauverworld/chat-space.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1' 
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['/home/ec2-user/.ssh/id_rsa']  
 
-# プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
-# Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
