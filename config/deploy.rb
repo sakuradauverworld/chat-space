702084ca98b0bc0e03537b1e4270171746757816
sakuradauverworld <sakuradaasaku@gmail.com>
2019-11-02 11:47:09 +0900

    uuu

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['uverworld.pem']  
+                  keys: ['~/.ssh/uverworld.pem']  
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
