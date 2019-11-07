f887ce48fb4cedef5bcf9776ace7fd60209a9ac6
sakuradauverworld <sakuradaasaku@gmail.com>
2019-11-07 20:39:34 +0900

    sa

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -69,7 +69,7 @@ $(function() {
     .parent()
     .remove();
   });
-  $(document).on("click", ".chat-group-user__btn", function() {
+  $(document).on("click",".chat-group-user__btn",".chat-group-user__btn--remove",function() {
     $(this)
     .parent()
     .remove();
